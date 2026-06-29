80b1a9ada3fc582dccba64de66f68bcf6fc7ac96
qork3m <gkara778@gmail.com>
2026-06-29 15:23:47 +0300

    Remove unused df_exchange_rates_monthly and df_merged variables

--- new10.py
+++ new10.py
@@ -23,28 +23,43 @@ def get_stock_data(ticker, start, end):
     return df
 
 
+# USD/TRY verilerini Yfinance API'ını kullanarak çekiyoruz.
 df_exchange_rates = get_stock_data('TRY=X', '2018-01-01', '2026-06-29')
+
+# TCMB politika faiz oranlarını EVDS API'ını kullanarak çekiyoruz.
 df_interest_rates = get_evds_series(
     api_key, ['TP.BISPOLFAIZ.TUR'], '01-01-2018', '29-06-2026')
-df_exchange_rates_monthly = df_exchange_rates['Close'].resample(
-    'MS').mean()
-df_merged = pd.merge(df_interest_rates, df_exchange_rates_monthly,
-                     right_index=True, left_index=True, how='inner')
 
+# volatilite DataFrame'ini hesaplıyoruz.
 volatility = df_exchange_rates['Close'].pct_change(1).rolling(30).std()
 
+# "diff" değişkenine bir önceki döneme göre politika faiz kararında bir değişiklik var mı onu atıyoruz.
 diff = df_interest_rates['TP_BISPOLFAIZ_TUR'].diff()
+
+# Yukarıdaki diff değişkenine atadıklarımızı "NaN" içermeyecek ve "diff" 0 olmayacak şekilde atıyoruz.
 df_interest_rate_change = df_interest_rates[(diff != 0) & (diff.notna())]
+
+# çıkan verilerin tarihlerini farklı bir DF'e atıyoruz.
 df_interest_rate_change_dates = df_interest_rate_change.index
 
+# Volatilite için çizgi grafiği çiziyoruz.
 plt.plot(volatility.index, volatility, color='blue')
+
+# Her bir endeks için tarihleri döndürüp dikey çizgi ekliyoruz o tarihlere.
 for tarih in df_interest_rate_change_dates:
     plt.axvline(x=tarih, color='red', linestyle='--', linewidth=1, alpha=0.5)
+
+# Grafiğin Başlığı
 plt.title("CBRT Rate Decisions and USD/TRY Volatility (2018-2026)",
           fontsize=14, fontweight='bold')
+
+# Grafiğin Y ekseni labelı
 plt.ylabel("USD/TRY 30-Day Rolling Volatility",
            color='blue', fontsize=11, fontstyle='italic')
+
+# Kaynağı belirtiyoruz.
 plt.figtext(0.5, 0.02, "Sources: TCMB, Yfinance",
             ha="center", fontsize=10, color="gray", fontstyle='italic')
+
 plt.tight_layout()
 plt.show()
